88b2de097bc88e68196d746271f108f247773649
vauxhall <zero.griffin@gmail.com>
2020-03-28 09:20:33 +0100

    Add tests

--- Sources/Git/Git.swift
+++ Sources/Git/Git.swift
@@ -2,17 +2,46 @@ import Foundation
 import Combine
 
 public final class Git {
+    private let queue = DispatchQueue(label: "", qos: .utility)
+    
     public init() { }
     
-    public func open(_ url: URL) -> Future<Repository, Fail.Repository> {
-        .init {
-            $0(.failure(.noRepository))
+    public func open(_ url: URL) -> Future<Repository, Error> {
+        .init { [weak self] promise in
+            self?.queue.async {
+                do {
+                    guard let repository = try self?.open(at: url) else { return }
+                    DispatchQueue.main.async {
+                        promise(.success(repository))
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        promise(.failure(error))
+                    }
+                }
+            }
         }
     }
     
     public func create(_ url: URL) -> Future<Repository, Error> {
-        .init { _ in
-            
+        .init { [weak self] promise in
+            self?.queue.async {
+                do {
+                    try self?.create(at: url)
+                } catch {
+                    promise(.failure(error))
+                }
+            }
         }
+    }
+    
+    private func open(at: URL) throws -> Repository {
+        throw Fail.Repository.noRepository
+    }
+    
+    private func create(at: URL) throws {
+        let root = at.appendingPathComponent(".git")
+        try FileManager.default.createDirectory(at: root, withIntermediateDirectories: false)
+        
     }
 }
--- Tests/Tests/CreateTests.swift
+++ Tests/Tests/CreateTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class CreateTests: Tests {
     func testCreate() {
         let expect = expectation(description: "")
+        
         let root = url.appendingPathComponent(".git")
         let refs = root.appendingPathComponent("refs")
         let objects = root.appendingPathComponent("objects")
@@ -16,6 +17,7 @@ final class CreateTests: Tests {
             default: XCTFail()
             }
         }) {
+            XCTAssertEqual(.main, Thread.current)
             XCTAssertEqual(self.url, $0.url)
             
             var dir = ObjCBool(false)
@@ -40,6 +42,55 @@ final class CreateTests: Tests {
             XCTAssertTrue(String(decoding: data ?? Data(), as: UTF8.self).contains("ref: refs/"))
             
             expect.fulfill()
+        }.store(in: &subs)
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testCreateNoAccess() {
+        let expect = expectation(description: "")
+        
+        git.create(URL(fileURLWithPath: "/")).sink(receiveCompletion: {
+            XCTAssertEqual(.main, Thread.current)
+            switch $0 {
+            case .failure(_):
+                expect.fulfill()
+            default: XCTFail()
+            }
+        }) { _ in
+            XCTFail()
+        }.store(in: &subs)
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testCreateNonExistingDirectory() {
+        let expect = expectation(description: "")
+        
+        git.create(url.appendingPathComponent("hello")).sink(receiveCompletion: {
+            switch $0 {
+            case .failure(_):
+                expect.fulfill()
+            default: XCTFail()
+            }
+        }) { _ in
+            XCTFail()
+        }.store(in: &subs)
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testCreateRemoteUrl() {
+        let expect = expectation(description: "")
+        
+        git.create(URL(string: "https://avocado.com")!).sink(receiveCompletion: {
+            switch $0 {
+            case .failure(_):
+                expect.fulfill()
+            default: XCTFail()
+            }
+        }) { _ in
+            XCTFail()
         }.store(in: &subs)
         
         waitForExpectations(timeout: 1)
