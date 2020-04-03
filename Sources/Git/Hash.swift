5734e40c6d8b8cfe50aea2e5f3b1c633a17ee050
vauxhall <zero.griffin@gmail.com>
2020-04-03 20:17:05 +0200

    Update hash

--- Sources/Git/Hash.swift
+++ Sources/Git/Hash.swift
@@ -6,17 +6,19 @@ final class Hash {
         blob(try! .init(contentsOf: url))
     }
     
-    class func blob(_ data: Data) -> Pack {
-        .init("blob \(data.count)\u{0000}".utf8 + data)
+    private class func blob(_ data: Data) -> Pack {
+        .init("blob", data: data)
     }
     
     struct Pack {
         let object: Data
         let hash: String
+        let size: Int
         
-        fileprivate init(_ object: Data) {
+        fileprivate init(_ prefix: String, data: Data) {
+            size = data.count
+            object = (prefix + " \(data.count)\u{0000}").utf8 + data
             hash = Insecure.SHA1.hash(data: object).compactMap { .init(format: "%02hhx", $0) }.joined()
-            self.object = object
         }
     }
 }
