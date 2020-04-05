86743858922fd20d2a024df59244ed4e3af6a69f
vauxhall <zero.griffin@gmail.com>
2020-04-05 18:16:01 +0200

    Parse index

--- Tests/IndexTests.swift
+++ Tests/IndexTests.swift
@@ -9,6 +9,29 @@ final class IndexTests: Tests {
         index = .init(url)
     }
     
+    func testLoad() {
+        try! FileManager.default.createDirectory(at: url.appendingPathComponent(".git"), withIntermediateDirectories: true)
+        try! Data(base64Encoded: index0)!.write(to: url.appendingPathComponent(".git/index"))
+        let items = index.items
+        XCTAssertEqual(1, items.count)
+        XCTAssertEqual("afile.json", items.first?.path)
+        XCTAssertEqual("3b18e512dba79e4c8300dd08aeb37f8e728b8dad", items.first?.hash)
+        XCTAssertEqual(12, items.first?.size)
+        XCTAssertEqual(1554190306, items.first?.created.timeIntervalSince1970)
+        XCTAssertEqual(1554190306, items.first?.modified.timeIntervalSince1970)
+        XCTAssertEqual(16777220, items.first?.device)
+        XCTAssertEqual(10051196, items.first?.inode)
+        XCTAssertEqual(502, items.first?.user)
+        XCTAssertEqual(20, items.first?.group)
+    }
+    
+    func testSave() {
+        try! FileManager.default.createDirectory(at: url.appendingPathComponent(".git"), withIntermediateDirectories: true)
+        try! Data(base64Encoded: index0)!.write(to: url.appendingPathComponent(".git/index"))
+        _ = index.save([])
+        XCTAssertEqual(index0, try? Data(contentsOf: url.appendingPathComponent(".git/index")).base64EncodedString())
+    }
+    
     func testAdd() {
         let file = url.appendingPathComponent("file.json")
         try! Data("hello world\n".utf8).write(to: file)
@@ -20,3 +43,5 @@ final class IndexTests: Tests {
         XCTAssertEqual("file.json", items.first?.path)
     }
 }
+
+private let index0 = "RElSQwAAAAIAAAABXKMP4g4nUXhcow/iDidReAEAAAQAmV58AACBpAAAAfYAAAAUAAAADDsY5RLbp55MgwDdCK6zf45yi42tAAphZmlsZS5qc29uAAAAAAAAAABIOjvvZZYKFlHYMWjy0VATl2F0cg=="
