a5772727575585a005818147fb9f385aa6307f5c
vauxhall <zero.griffin@gmail.com>
2020-04-12 12:10:05 +0200

    Serialise commit

--- Sources/Log.swift
+++ Sources/Log.swift
@@ -3,6 +3,11 @@ import Combine
 
 public final class Log {
     weak var repository: Repository!
+    private let timezone = DateFormatter()
+    
+    init() {
+        timezone.dateFormat = "xx"
+    }
     
     public var history: Future<Commit?, Never> {
         .init { [weak self] promise in
@@ -14,9 +19,15 @@ public final class Log {
         }
     }
     
-    public func commit(_ paths: [String], message: String) {
-        repository.queue.async {
-            
+    public func commit(_ paths: Set<String>, message: String) {
+        repository.queue.async { [weak self] in
+            guard let self = self else { return }
+            let author = Git.credentials.name +  " <" + Git.credentials.email +  "> \(Int(Date().timeIntervalSince1970)) " + self.timezone.string(from: .init())
+            var serial = "tree " + self.repository.index.save(paths).hash
+            serial += "\nauthor " + author
+            serial += "\ncommitter " + author
+            serial += "\n\n" + message
+            let pack = Hash.commit(serial)
         }
     }
 }
--- Tests/LogTests.swift
+++ Tests/LogTests.swift
@@ -1,8 +1,14 @@
 import XCTest
-import Git
+@testable import Git
 
 final class LogTests: Tests {
     var repository: Repository!
+    
+    override func setUp() {
+        super.setUp()
+        Git.credentials.name = "Cez Berenjena"
+        Git.credentials.email = "cez@berenjena.com"
+    }
     
     func testNoCommits() {
         let expect = expectation(description: "")
@@ -27,6 +33,8 @@ final class LogTests: Tests {
             self.repository.log.commit(["file.txt"], message: "first commit")
             self.repository.log.history.sink {
                 XCTAssertEqual("first commit", $0?.message)
+                XCTAssertEqual("file.txt", self.repository.index.items.first?.path)
+                XCTAssertNil($0?.parent)
                 expect.fulfill()
             }.store(in: &self.subs)
         }.store(in: &subs)
